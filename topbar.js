4b6f13b2da68f1457eed757120f1c0a85c4dc46a
Ashwin Gupta <ashwin.guptan@gmail.com>
2025-10-04 16:56:41 +0530

    Updated topbar

--- topbar.js
+++ topbar.js
@@ -1,3 +1,4 @@
+var text_size; 
 function topbar_setup() {
 
 }
@@ -7,5 +8,14 @@ function topbar_update() {
 }
 
 function topbar_draw() {
+	let mytext="ASTROVIEW";
+	text_size=textWidth(mytext);
+	fill(255,255,255,200);
+	rect(0,0,windowWidth,60);
+	fill('Black');
+	text(mytext,(windowWidth/2)-(text_size/2),40);
+
+	
+
 	topbar_update()
 }
