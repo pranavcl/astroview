d945f92744ac349de2153c426acc851bd5253c44
vj031206 <eternalelate@gmail.com>
2025-10-04 16:20:50 +0530

    Added topbar

--- topbar.js
+++ topbar.js
@@ -1,3 +1,5 @@
+var text_size;
+
 function topbar_setup() {
 
 }
@@ -7,5 +9,12 @@ function topbar_update() {
 }
 
 function topbar_draw() {
+	let mytext="ASTROVIEW";
+	text_size=textWidth(mytext);
+	fill(255,255,255,200);
+	rect(0,0,windowWidth,60);
+	fill('Black');
+	text(mytext,(windowWidth/2)-(text_size/2),40);
+
 	topbar_update()
 }
