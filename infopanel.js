b2aec95f7283387f46755d0191411cb01a45bcb9
Your Name <you@example.com>
2025-10-04 14:33:24 +0530

    Info panel add box and heading

--- infopanel.js
+++ infopanel.js
@@ -7,5 +7,12 @@ function infopanel_update() {
 }
 
 function infopanel_draw() {
+
+	fill(255,255,255,200)
+	rect(windowWidth-250,windowHeight-500,190,300);
+	fill(0,0,0);
+	textFont("Instrumental Sans", 30);
+	text("Info",windowWidth-230,windowHeight-460);
+
 	infopanel_update();
 }
