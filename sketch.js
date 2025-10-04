95e8dc9d2f11069b341f9b83ee68bfba19f973dc
Your Name <you@example.com>
2025-10-04 23:35:28 +0530

    Fixed stroke bug again

--- sketch.js
+++ sketch.js
@@ -230,7 +230,6 @@ function draw() {
 	textSize(18);
 	var pt = viewer.viewport.pointFromPixel(new OpenSeadragon.Point(mouseX, mouseY));
 	text("Zoom level: " + viewer.viewport.getZoom() + "\nImage X: " + parseInt(pt.x*imageWidth) + " | Image Y: " + parseInt(pt.y*imageWidth) + "\nX: " + mouseX + " | Y: " + mouseY + "\nTool: " + tool, 20, windowHeight-100);
-sk
 	rect(toolbar.x, toolbar.y, toolbar.width, toolbar.height,5);
 
 	for(var i in tools) {
