c738337f7a7bc22655331162e52ddaa195c51826
vj031206 <eternalelate@gmail.com>
2025-10-04 22:39:01 +0530

    Completed info panel

--- infopanel.js
+++ infopanel.js
@@ -1,19 +1,15 @@
-var boldFont;
-var normalFont;
-var milky_way;
-var forward;
+var textstyle2;
+var textstyle3;
 
 var infopanel = {
 	width: 230,
 	height: 300
 };
-var padx = 32;
+var padx = 30;
 
 function infopanel_setup() {
-	boldFont = loadFont("/fonts/ARIALBD.ttf");
-	normalFont = loadFont("/fonts/ARIAL.ttf");
-	milky_way=loadImage("/images/milky_way.png");
-	forward=loadImage("/images/next.png");
+	textstyle2 = loadFont("images/Instrument_Sans/InstrumentSans-VariableFont_wdth,wght.ttf");
+	textstyle3 = loadFont("images/Instrument_Sans/static/InstrumentSans-Bold.ttf");
 }
 
 function infopanel_update() {
@@ -23,19 +19,20 @@ function infopanel_update() {
 function infopanel_draw() {
 	fill(255,255,255,190);
 	infopanel.x = windowWidth-infopanel.width;
-	infopanel.y = windowHeight/2-infopanel.height/2;
-	rect(infopanel.x, infopanel.y, infopanel.width, infopanel.height-160);
+	infopanel.y = windowHeight/2-260;
+
+	rect(infopanel.x, infopanel.y, infopanel.width - 50, infopanel.height-180,5);
 	fill(0,0,0);
-	textFont(boldFont);
+	textFont(textstyle3);
+
 	textSize(24);
-	image(forward,infopanel.x+padx/2, infopanel.y+padx*1-12, 16, 16);
-	text("    Info Panel",infopanel.x+padx, infopanel.y+padx/2);
-	textFont(normalFont);
-    textSize(16);
-	text("    Image Name:Image1.jpeg",infopanel.x+padx/2, infopanel.y+padx*2-12);
-	image(milky_way, infopanel.x+padx/2, infopanel.y+padx*2.5-26, 16, 16);
-	text("    ImageSize :9.7MB ",infopanel.x+padx/2, infopanel.y+padx*2.5);
-	text("    Window Width: 10552 bytes",infopanel.x+padx/2, infopanel.y+padx*3.1);
-	text(" Window Height:2468 bytes",infopanel.x+padx/1.01, infopanel.y+padx*3.7);
+	text("Info Panel",infopanel.x + padx/2, infopanel.y + 10);
+
+	textFont(textstyle2);
+    textSize(13);
+	text("Image1.jpeg",infopanel.x+padx/2, infopanel.y+50);
+	text("9.7MB ",infopanel.x+padx/2, infopanel.y+70);
+	text("10552 x 2468 pixels",infopanel.x+padx/2, infopanel.y+90);
+
 	infopanel_update();
 }
