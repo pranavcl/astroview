9fb0da8f16d92a3bae463ded5f1a874478051edd
Your Name <you@example.com>
2025-10-04 12:26:35 +0530

    adding rect

--- zoomtools.js
+++ zoomtools.js
@@ -1,8 +1,26 @@
+// let zoomIn;
+// let zoomOut;
+// let resetZoom;
+
 function zoomtools_update() {
 
 }
 
 function zoomtools_draw() {
-	// fill(0, 255, 0);
-	// rect(32, 32, 32, 32);
+
+	fill(0, 255, 0);
+
+	rect(1260, 660, 32, 32);
+	rect(1300, 660, 32, 32);
+	rect(1340, 660, 32, 32);
+
+	rect(zoombar.x, zoombar.y, zoombar.width, zoombar.height);
+
+	// zoomIn = createButton('zoom in');
+	// zoomIn.position(1260, 660)
+	// zoomIn.position(1000,1900);
+	// zoomOut = createButton('zoom out');
+	// resetZoom = createButton('zoom out');
+
+	// zoom_in.mousePressed(repaint);
 }
